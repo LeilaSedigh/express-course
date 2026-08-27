5a8576c671282183e09d2432b8c858e7aa50eb8b
leila <leilasedighbakhshayesh@gmail.com>
2026-08-27 11:56:44 +0330

    .

--- src/controllers/general.mjs
+++ src/controllers/general.mjs
@@ -1,11 +1,30 @@
-import path from 'path'
+import path from "path";
+import fs from "fs";
+import { title } from "process";
 
-export function home ( req, res ) {
-    const filePath = path.resolve(import.meta.dirname, '..', 'views', 'index.html')
+function renderTemplate(parms) {
+  const filePath = path.resolve(
+    import.meta.dirname,
+    "..",
+    "views",
+    "index.html",
+  );
 
-    res.sendFile(filePath);
+  let view = fs.readFileSync(filePath, "utf-8");
+  console.log("view", view);
+
+  const entries = Object.entries(parms);
+  entries.forEach(([key , value]) => {
+    view = view.replace(`#${key}#` , value)
+  })
+
+  return view;
 }
 
-export function about ( req, res ) {
-    res.send("About us")
+export function home(req, res) {
+  res.send(renderTemplate({ title: "Home age", content: "This is Home Page" }));
+}
+
+export function about(req, res) {
+  res.send(renderTemplate({ title: "About age", content: "This is About Page" }));
 }
