467487e47aba489055f645bc597c9471a709f9c9
leila <leilasedighbakhshayesh@gmail.com>
2026-09-06 13:15:56 +0330

    .

--- src/controllers/admin/article.mjs
+++ src/controllers/admin/article.mjs
@@ -1,10 +1,31 @@
 class ArticleController {
-    list(req, res ) {
-        res.render("admin/article/list" , {
+    list(req, res) {
+        res.render("admin/article/list", {
             title: "Article List",
-            content: "Salam"
+            articles: [
+                { id: 1, title: "Article" },
+                { id: 2, title: "Article 2" },
+            ],
+        });
+
+    }
+    get(req, res) {
+        const article = {
+            id: 1,
+            title: "Article",
+            text: "Salam man de to yar ghadimi"
+        }
+
+        res.render("admin/article/detail", {
+            title: article.title,
+            article
+        })
+    }
+    create(req ,res){
+        res.render('admin/article/create' ,{
+            title: "Create new Article"
         })
     }
 }
 
-export default new ArticleController()
+export default new ArticleController();
--- src/routes/admin/article.mjs
+++ src/routes/admin/article.mjs
@@ -4,6 +4,8 @@ import ArticleController from "../../controllers/admin/article.mjs"
 const router = express.Router()
 
 router.get("/", ArticleController.list)
+router.get("/:id", ArticleController.get)
+router.get("/create", ArticleController.create)
 
 
 export default router
