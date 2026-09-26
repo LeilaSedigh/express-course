bb59418d0d8ca968e3a401d1838a983509becee0
leila <leilasedighbakhshayesh@gmail.com>
2026-09-26 14:43:25 +0330

    .

--- src/config/database.mjs
+++ src/config/database.mjs
@@ -1,12 +1,12 @@
 import { Sequelize } from "sequelize";
 
 export const sequelize = new Sequelize(
-  "blog",
-  "root",
-  "12345678",
+  process.env.DATABASE_NAME,
+   process.env.DATABASE_USER,
+   process.env.DATABASE_PASSWORD,
   {
     dialect: "mysql",
-    port: "3307",
-    host: "localhost"
+    port: process.env.DATABASE_PORT,
+    host: process.env.DATABASE_HOST
   }
 );
